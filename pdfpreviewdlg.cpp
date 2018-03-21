749362c6f42c6ee56b50af7a4e122e68abdc5519
Jean GUARDIA <jean.guardia@orange.fr>
2018-03-21 15:16:59 +0100

    Pdf preview works as expected. Still missing delete capability

--- pdfpreviewdlg.cpp
+++ pdfpreviewdlg.cpp
@@ -10,6 +10,7 @@ PdfPreviewDlg::PdfPreviewDlg(QList<QImage> images, QWidget* parent)
     if (_previewImages.size() > 0) {
         _currentIndex = 0;
         _imageCount = _previewImages.size();
+        displayImage();
     } else {
         _currentIndex = -1;
         _imageCount = -1;
@@ -36,16 +37,21 @@ void PdfPreviewDlg::updateGui()
         ui->btnLast->setEnabled(false);
         ui->btnNext->setEnabled(false);
         ui->btnPrevious->setEnabled(false);
-    } else if (_currentIndex == 0) {
+    } else if (_currentIndex == 0 && _imageCount - 1 > 0) {
         ui->btnFirst->setEnabled(false);
         ui->btnLast->setEnabled(true);
         ui->btnNext->setEnabled(true);
         ui->btnPrevious->setEnabled(false);
-    } else if (_currentIndex == _imageCount - 1 && _imageCount > 0) {
+    } else if (_currentIndex == _imageCount - 1 && _imageCount > 1) {
         ui->btnFirst->setEnabled(true);
         ui->btnLast->setEnabled(false);
         ui->btnNext->setEnabled(false);
         ui->btnPrevious->setEnabled(true);
+    } else if (_currentIndex == 0 && _imageCount == 1) {
+        ui->btnFirst->setEnabled(false);
+        ui->btnLast->setEnabled(false);
+        ui->btnNext->setEnabled(false);
+        ui->btnPrevious->setEnabled(false);
     } else {
         ui->btnFirst->setEnabled(true);
         ui->btnLast->setEnabled(true);
@@ -57,7 +63,10 @@ void PdfPreviewDlg::updateGui()
 void PdfPreviewDlg::displayImage()
 {
     if (_currentIndex >= 0 && _currentIndex < _imageCount) {
-        ui->lblShowImage->setPixmap(QPixmap::fromImage(_previewImages.at(_currentIndex)));
+        QPixmap imageToDisplay = QPixmap::fromImage(_previewImages.at(_currentIndex));
+        QPixmap finalImage = imageToDisplay.scaledToHeight(700);
+        ui->lblShowImage->setPixmap(finalImage);
+        updateGui();
     }
 }
 
